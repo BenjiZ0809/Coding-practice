0b7b66ccb0cea7c47ec3917c09a67e00eed08cbc
BenjiZ0809 <84465629+BenjiZ0809@users.noreply.github.com>
2022-08-31 17:06:59 -0400

    Update 5. Longest Palindromic Substring.java

--- 5. Longest Palindromic Substring.java
+++ 5. Longest Palindromic Substring.java
@@ -1,3 +1,4 @@
+//expand() helper function approach
 class Solution {
     public String longestPalindrome(String s) {
         int start = 0;
@@ -20,5 +21,40 @@ class Solution {
             r++;
         }
         return r - l -1;
+    }
+}
+
+
+//DP approach
+class Solution {
+    public String longestPalindrome(String s) {
+        if(s == null || s.equals("")) return "";
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        
+        int longest = 1;
+        int start = 0;
+        for(int i=0; i<n; i++){
+            dp[i][i] = true;
+        }
+        
+        for(int i=0; i<n-1; i++){
+            dp[i][i+1] = s.charAt(i) == s.charAt(i+1);
+            if(dp[i][i+1]){
+                longest = 2;
+                start = i;
+            }
+        }
+        
+        for(int i=n-1; i>=0; i--){
+            for(int j=i+2; j<n; j++){
+                dp[i][j] = dp[i+1][j-1] && s.charAt(i) == s.charAt(j);
+                if(dp[i][j] && j - i + 1 > longest){
+                    longest = j - i + 1;
+                    start = i;
+                }
+            }
+        }
+        return s.substring(start, start + longest);
     }
 }
