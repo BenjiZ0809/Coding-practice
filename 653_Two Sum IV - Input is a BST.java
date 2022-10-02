557714aa198c56e2d19f6260f661a541b3bdff44
Benjamin <84465629+BenjiZ0809@users.noreply.github.com>
2022-10-02 17:10:33 -0400

    Update 653_Two Sum IV - Input is a BST.java

--- 653_Two Sum IV - Input is a BST.java
+++ 653_Two Sum IV - Input is a BST.java
@@ -15,6 +15,7 @@
  */
 class Solution {
     Set<Integer> set = new HashSet<>();
+    
     public boolean findTarget(TreeNode root, int k) {
         if(root == null) return false;
         if(set.contains(k - root.val)) return true;
