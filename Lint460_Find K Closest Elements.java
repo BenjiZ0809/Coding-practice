e28ff9560e8f6f4893c3983dacb8c39afd7b0105
Benjamin <84465629+BenjiZ0809@users.noreply.github.com>
2022-10-04 17:00:52 -0400

    Create Lint460_Find K Closest Elements.java

---
+++ Lint460_Find K Closest Elements.java
@@ -0,0 +1,50 @@
+public class Solution {
+    /**
+     * @param a: an integer array
+     * @param target: An integer
+     * @param k: An integer
+     * @return: an integer array
+     */
+    public int[] kClosestNumbers(int[] a, int target, int k) {
+        int right = binarySearch(a, target);
+        int left = right - 1;
+
+        int index = 0;
+        int[] res = new int[k];
+        while(index < k) {
+            if(isLeftCloser(a, target, left, right)) {
+                res[index++] = a[left--];
+            }
+            else {
+                res[index++] = a[right++];
+            }
+        }
+        return res;
+    }
+
+    private boolean isLeftCloser(int[]nums, int target, int left, int right) {
+        if(left < 0 ) return false;
+        if(right >= nums.length) return true;
+        return target - nums[left] <= nums[right] - target;
+    }
+
+    private int binarySearch(int[] nums, int target) {
+        //if(nums == null || nums.length == 0) return -1;
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left + 1 < right) {
+            System.out.println(left);
+            System.out.println(right);
+            int mid = left + (right - left) / 2;
+            if(nums[mid] < target) {
+                left = mid;
+            }
+            else right = mid;
+        }
+
+        if(nums[left] >= target) return left;
+        if(nums[right] >= target) return right;
+        return nums.length;
+    }
+}
