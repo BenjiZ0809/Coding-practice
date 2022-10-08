806e76d20bf7921118faf9abb8b7f8de607e1efc
Benjamin <84465629+BenjiZ0809@users.noreply.github.com>
2022-10-08 15:18:13 -0400

    Update 34_Find First and Last Position of Element in Sorted Array.java

--- 34_Find First and Last Position of Element in Sorted Array.java
+++ 34_Find First and Last Position of Element in Sorted Array.java
@@ -1,3 +1,50 @@
+//2 binary search approach
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] res = new int[2];
+        
+        res[0] = findFirst(nums, target);
+        res[1] = findLast(nums, target);
+        
+        return res;
+    }
+    
+    private int findFirst(int[] nums, int target) {
+        if(nums.length == 0 || nums == null) return -1;
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while(left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] >= target) right = mid;
+            else left = mid;
+        }
+        
+        if(nums[left] == target) return left;
+        if(nums[right] == target) return right;
+        return -1;
+    }
+    
+    private int findLast(int[] nums, int target) {
+        if(nums.length == 0 || nums == null) return -1;
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while(left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] <= target) left = mid;
+            else right = mid;
+        }
+        
+        if(nums[right] == target) return right;
+        if(nums[left] == target) return left;
+        return -1;
+    }
+}
+
+
+
+//1 binary search + 2 pointer O(logn + k), k = number of elements equals to target
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int[] res = new int[]{-1, -1};
