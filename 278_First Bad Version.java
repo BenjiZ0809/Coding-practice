3c83330cd2cc0f8d7d9eb2140d97033da8c5a808
Benjamin <84465629+BenjiZ0809@users.noreply.github.com>
2022-10-08 16:28:11 -0400

    Create 278_First Bad Version.java

---
+++ 278_First Bad Version.java
@@ -0,0 +1,15 @@
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        
+        while(left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if(isBadVersion(mid)) right = mid;
+            else left = mid;
+        }
+        
+        if(isBadVersion(left)) return left;
+        else return right;
+    }
+}
