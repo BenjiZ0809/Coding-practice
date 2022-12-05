763189136a0e95f4b1d87d518773681375acb899
Benjamin <84465629+BenjiZ0809@users.noreply.github.com>
2022-12-05 11:45:04 -0500

    Update 513_Find Bottom Left Tree Value.java

--- 513_Find Bottom Left Tree Value.java
+++ 513_Find Bottom Left Tree Value.java
@@ -1,3 +1,4 @@
+//BFS
 class Solution {
     public int findBottomLeftValue(TreeNode root) {
         Queue<TreeNode> q = new ArrayDeque<>();
@@ -9,5 +10,24 @@ class Solution {
             if(root.left != null) q.offer(root.left);
         }
         return root.val;
+    }
+}
+
+//DFS
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        traverse(root, 1);
+        return res;
+    }
+    int level = 0;
+    int res = 0;
+    private void traverse(TreeNode root, int lv) {
+        if(root == null) return;
+        if(lv > level) {
+            res = root.val;
+            level = lv;
+        }
+        traverse(root.left, lv + 1);
+        traverse(root.right, lv + 1);
     }
 }
