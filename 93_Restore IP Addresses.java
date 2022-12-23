070aa292afc259fbb5f6f372f505e534da5f9e76
Benjamin <84465629+BenjiZ0809@users.noreply.github.com>
2022-12-23 16:38:59 -0500

    Update 93_Restore IP Addresses.java

--- 93_Restore IP Addresses.java
+++ 93_Restore IP Addresses.java
@@ -14,6 +14,7 @@ class Solution {
                 return;
             }
         }
+        if(num > 3) return;
 
         for(int i=index; i<s.length(); i++) {
             if(isValid(s, index, i)) {
