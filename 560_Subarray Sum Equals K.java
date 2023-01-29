fdce7067f8f52c4ef7a9f0427656bdb082d44e81
Benjamin <84465629+BenjiZ0809@users.noreply.github.com>
2023-01-29 10:41:59 -0500

    Update 560_Subarray Sum Equals K.java

--- 560_Subarray Sum Equals K.java
+++ 560_Subarray Sum Equals K.java
@@ -1,12 +1,12 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
         int res = 0;
         int sum = 0;
-        
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
         for(int i=0; i<nums.length; i++) {
             sum += nums[i];
-            if(sum == k) res++;
             if(map.containsKey(sum - k)) {
                 res += map.get(sum - k);
             }
