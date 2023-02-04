f00b144646d2fddec76221534bd270290e5fa9c8
Benjamin <84465629+BenjiZ0809@users.noreply.github.com>
2023-02-04 14:47:06 -0500

    Update 480_Sliding Window Median.java

--- 480_Sliding Window Median.java
+++ 480_Sliding Window Median.java
@@ -70,3 +70,42 @@ class Solution {
         window[j] = val;
     }
 }
+
+
+//PQ
+class Solution {
+    public double[] medianSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        Queue<Integer> min = new PriorityQueue<>();
+        Queue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
+
+        double[] res = new double[n - k + 1];
+        int index = 0, i = 0, j = 0;
+
+        while(j < n) {
+            if(min.size() <= max.size()) {
+                max.offer(nums[j]);
+                min.offer(max.poll());
+            }
+            else {
+                min.offer(nums[j]);
+                max.offer(min.poll());
+            }
+
+            if(j - i + 1 < k) j++;
+            else if(j - i + 1 == k) {
+                if(min.size() == max.size()) {
+                    res[index++] = ((long)min.peek() + (long)max.peek()) / 2.0;
+                }
+                else {
+                    res[index++] = (double)min.peek();
+                }
+                if(min.contains(nums[i])) min.remove(nums[i]);
+                else max.remove(nums[i]);
+                i++;
+                j++;
+            }
+        }
+        return res;
+    }
+}
