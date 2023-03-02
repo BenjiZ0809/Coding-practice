6c605c061c563ed1184c316ea6dffe7ec910aadb
Benjamin <84465629+BenjiZ0809@users.noreply.github.com>
2023-03-02 11:34:41 -0500

    Update 121_Best Time to Buy and Sell Stock.java

--- 121_Best Time to Buy and Sell Stock.java
+++ 121_Best Time to Buy and Sell Stock.java
@@ -22,7 +22,7 @@ class Solution {
     }
 }
 
-//dp?
+//solution 2
 class Solution {
     public int maxProfit(int[] prices) {
         int[] dp = new int[prices.length];
@@ -39,7 +39,7 @@ class Solution {
     }
 }
 
-//dp2?
+//solution 3
 class Solution {
     public int maxProfit(int[] prices) {
         int min = Integer.MAX_VALUE;
@@ -50,5 +50,19 @@ class Solution {
             res = Math.max(res, prices[i] - min);
         }
         return res;
+    }
+}
+
+//DP
+class Solution {
+    public int maxProfit(int[] prices) {
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] = -prices[0];
+
+        for(int i=1; i<prices.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], -prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+        }
+        return Math.max(dp[prices.length - 1][0], dp[prices.length - 1][1]);
     }
 }
