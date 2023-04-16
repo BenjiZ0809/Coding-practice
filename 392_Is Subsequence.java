4291566f0ee49780285cca111173ef73c13583ce
Benjamin <84465629+BenjiZ0809@users.noreply.github.com>
2023-04-16 15:05:15 -0400

    Update 392_Is Subsequence.java

--- 392_Is Subsequence.java
+++ 392_Is Subsequence.java
@@ -11,6 +11,30 @@ class Solution {
                 right++;
             }
         }
-        return left > s.length() - 1;
+        return left == s.length();
+    }
+}
+
+// DP approach
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        boolean[][] dp = new boolean[n + 1][m + 1];
+        for(int j=0; j<=m; j++) {
+            dp[0][j] = true;
+        }
+
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=m; j++) {
+                if(s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else {
+                    dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+        return dp[n][m];
     }
 }
