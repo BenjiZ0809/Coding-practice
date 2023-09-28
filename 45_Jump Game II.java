bbc7ea051894f88d4dde318667a54d5e1642ce1f
Xi Zhou <84465629+BenjiZ0809@users.noreply.github.com>
2023-09-28 11:29:14 -0400

    Update 45_Jump Game II.java

--- 45_Jump Game II.java
+++ 45_Jump Game II.java
@@ -1,3 +1,4 @@
+// greedy (fast)
 class Solution {
     public int jump(int[] nums) {
         int n = nums.length;
@@ -13,5 +14,25 @@ class Solution {
             res++;
         }
         return res;
+    }
+}
+
+
+// dp (slow)
+lass Solution {
+    public int jump(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for(int i=1; i<n; i++) {
+            for(int j=0; j<i; j++) {
+                if(nums[j] + j >= i) {
+                    dp[i] = Math.min(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return dp[n - 1];
     }
 }
