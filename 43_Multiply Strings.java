d5ed3e854aa4fe5bb2866a2409e31f93151cb9df
Xi Zhou <84465629+BenjiZ0809@users.noreply.github.com>
2023-10-09 10:08:41 -0400

    Update 43_Multiply Strings.java

--- 43_Multiply Strings.java
+++ 43_Multiply Strings.java
@@ -1,3 +1,4 @@
+// approach 1
 class Solution {
     public String multiply(String num1, String num2) {
         if(num1.equals("0") || num2.equals("0")) return "0";
@@ -26,5 +27,38 @@ class Solution {
         }
         
         return str;
+    }
+}
+
+// approach 2
+class Solution {
+    public String multiply(String num1, String num2) {
+        if(num1.equals("0") || num2.equals("0")) return "0";
+        int n = num1.length();
+        int m = num2.length();
+        int[] res = new int[m + n];
+
+        for(int i=n-1; i>=0; i--) {
+            for(int j=m-1; j>=0; j--) {
+                int p1 = i + j;
+                int p2 = i + j + 1;
+                int num = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                num += res[p2];
+                res[p1] += num / 10;
+                res[p2] = num % 10;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        if(res[0] == 0) {
+            for(int i=1; i<m+n; i++) {
+                sb.append(res[i]);
+            }
+            return sb.toString();
+        }
+        for(int i=0; i<m+n; i++) {
+            sb.append(res[i]);
+        }
+        return sb.toString();
     }
 }
