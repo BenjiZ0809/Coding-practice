188d23d92e3d49c6651a2521f86c56e1de6b359a
Xi Zhou <84465629+BenjiZ0809@users.noreply.github.com>
2024-01-28 16:07:57 -0500

    Update 88_Merge Sorted Array.java

--- 88_Merge Sorted Array.java
+++ 88_Merge Sorted Array.java
@@ -1,3 +1,21 @@
+// Better approach
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int l = m - 1;
+        int r = n - 1;
+        int index = m + n - 1;
+        while(l >= 0 && r >= 0) {
+            if(nums1[l] > nums2[r]) nums1[index--] = nums1[l--];
+            else nums1[index--] = nums2[r--];
+        }
+
+        while(l >= 0) nums1[index--] = nums1[l--];
+        while(r >= 0) nums1[index--] = nums2[r--];
+    }
+}
+
+
+// Use extra space
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int left = 0;
