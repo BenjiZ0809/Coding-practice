1ff0f844aff7698caec7f901dd39576a7fc89c0b
Xi Zhou <84465629+BenjiZ0809@users.noreply.github.com>
2026-01-22 19:40:21 -0800

    Update 567_Permutation in String.java

--- 567_Permutation in String.java
+++ 567_Permutation in String.java
@@ -1,20 +1,22 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int len1 = s1.length();
-        int len2 = s2.length();
-        int[] hash1 = new int[26];
-        int[] hash2 = new int[26];
-        
-        for(int i=0; i<len1; i++) {
-            hash1[s1.charAt(i) - 'a']++;
-        }
-        
-        for(int i=0; i<len2; i++) {
-            if(i >= len1) {
-                hash2[s2.charAt(i - len1) - 'a']--;
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
+        for(char ch:s1.toCharArray()) arr1[ch - 'a']++;
+        int l = 0;
+        int r = 0;
+        while(r < s2.length()) {
+            char ch = s2.charAt(r);
+            arr2[ch - 'a']++;
+            while(r - l + 1 > s1.length()) {
+                char del = s2.charAt(l);
+                arr2[del - 'a']--;
+                l++;
             }
-            hash2[s2.charAt(i) - 'a']++;
-            if(Arrays.equals(hash1, hash2)) return true;
+            if(r - l + 1 == s1.length()) {
+                if (Arrays.equals(arr1, arr2)) return true;
+            }
+            r++;
         }
         return false;
     }
